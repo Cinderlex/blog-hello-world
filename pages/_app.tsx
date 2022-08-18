275c258a44091a5947edaed5cfa075a973aeb093
Alex Ash <streamprop@gmail.com>
2022-08-18 17:25:21 +0300

    Cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+      <>
+        <Head>
+          <meta name="description" content="First steps with building blogs" />
+          <title>Hello World Blog</title>
+          <link rel="icon" href="/favicon.png" />
+        </Head>
+        <Component {...pageProps} />
+      </>
+  )
 }
 
 export default MyApp
